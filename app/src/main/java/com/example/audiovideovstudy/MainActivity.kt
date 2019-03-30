512e9618e0dd35bdba6e2c6b0e55c2fb67439b58
Administrator <489310600@qq.com>
2019-03-30 12:36:16 +0800

    测试

--- app/src/main/java/com/example/audiovideovstudy/MainActivity.kt
+++ app/src/main/java/com/example/audiovideovstudy/MainActivity.kt
@@ -6,6 +6,8 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
